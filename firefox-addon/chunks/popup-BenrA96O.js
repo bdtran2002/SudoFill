e324f068673cab92bd2f8a5fc57d981219a95340
Kamisato <bdtran2002@gmail.com>
2026-04-27 00:09:10 -0700

    refresh firefox addon snapshot

---
+++ firefox-addon/chunks/popup-BenrA96O.js
@@ -0,0 +1,1 @@
+import{g as e,x as t}from"./settings-BfRVkBus.js";import{t as n}from"./mailbox-app-BDTvvXRG.js";var r=t(),i=e();(0,r.createRoot)(document.getElementById(`root`)).render((0,i.jsx)(n,{}));
